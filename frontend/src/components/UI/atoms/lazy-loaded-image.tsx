627ef69b9d8600c32c1ead857bb7300dc4a4dee2
1ethansempa <esempa@sailglobalcorp.com>
2023-06-01 14:23:11 +0300

    build: route not found page

--- frontend/src/components/UI/atoms/lazy-loaded-image.tsx
+++ frontend/src/components/UI/atoms/lazy-loaded-image.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { InputProps } from "../../../common/types";
 
-interface LazyLoadedImageProps {
+interface LazyLoadedImageProps extends InputProps {
   src: string;
   alt: string;
 }
 
-function LazyLoadedImage({ src, alt }: LazyLoadedImageProps) {
-  return <img src={src} alt={alt} loading="lazy" />;
+function LazyLoadedImage({ src, alt, className }: LazyLoadedImageProps) {
+  return <img src={src} alt={alt} loading="lazy" className={className} />;
 }
 
 export default LazyLoadedImage;
--- frontend/src/components/pages/not-found-page.tsx
+++ frontend/src/components/pages/not-found-page.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import LazyLoadedImage from "../UI/atoms/lazy-loaded-image";
+import PrimaryButton from "../UI/atoms/primary-button";
+import { useNavigate } from "react-router-dom";
 
 function NotFoundPage() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1 className="text-3xl text-white">Oops Page Doesn't Exist</h1>
+    <div className="p-8">
+      <div className="flex items-center flex-col justify-center mt-16">
+        <LazyLoadedImage src="404.png" alt="Page Not Found" className="h-96" />
+        <div className="text-sm my-3">
+          It appears the page you're looking for doesn't exist.
+        </div>
+        <PrimaryButton
+          text="Go to Home"
+          className="text-sm py-4 px-6"
+          includeArrow={true}
+          clickAction={() => {
+            navigate("/", { replace: true });
+          }}
+        />
+      </div>
     </div>
   );
 }
